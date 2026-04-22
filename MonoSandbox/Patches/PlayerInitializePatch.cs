33a7fb16d13383c970c2e74d80f3c2e1adb8c8e1
Exist <existlol@yahoo.com>
2026-04-22 18:17:40 +0200

    goon

--- MonoSandbox/Patches/PlayerInitializePatch.cs
+++ MonoSandbox/Patches/PlayerInitializePatch.cs
@@ -1,10 +1,16 @@
+using System.Reflection;
 using HarmonyLib;
 
 namespace MonoSandbox.Patches
 {
-    [HarmonyPatch(typeof(GorillaTagger), "Start")]
+    [HarmonyPatch]
     public class PlayerInitializePatch
     {
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(typeof(GorillaTagger), "Start");
+        }
+
         public static void Postfix(GorillaTagger __instance)
         {
             RefCache.LHand = __instance.offlineVRRig.leftHandTransform.parent.Find("palm.01.L").gameObject;
